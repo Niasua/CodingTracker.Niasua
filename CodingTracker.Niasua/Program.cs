a9b5ff9818cc37311870df320eb23163e93ae0fb
niasua <nsuar@DESKTOP-G227FH7>
2025-07-15 19:12:52 -0300

    the menu is displayed correctly

--- CodingTracker.Niasua/Program.cs
+++ CodingTracker.Niasua/Program.cs
@@ -1,3 +1,41 @@
 ﻿using CodingTracker.Niasua.Data;
+using CodingTracker.Niasua.Models;
+using CodingTracker.Niasua.UI;
+using Spectre.Console;
+using SQLitePCL;
+
+Batteries.Init();
 
 CodingController.InitializeDatabase();
+
+bool exit = false;
+
+while (!exit)
+{
+    Console.Clear();
+    AnsiConsole.MarkupLine("[bold blue]Coding Tracker[/]");
+    AnsiConsole.MarkupLine("Select an option:\n");
+
+    var choice = AnsiConsole.Prompt(
+        new SelectionPrompt<string>()
+        .Title("Choose an [green]action[/].")
+        .AddChoices(new[]
+        {
+            "View all sessions",
+            "Add new session",
+            "Exit"
+        }));
+
+    switch (choice)
+    {
+        case "View all sessions":
+            var sessions = CodingController.GetAllSessions();
+            TableDisplay.ShowSessions(sessions);
+            break;
+        case "Add new session":
+            break;
+        case "Exit":
+            exit = true;
+            break;
+    }
+}
