6cfcef83223b4daca5d3f1f96bfe2a5736eafb4c
niasua <nsuar@DESKTOP-G227FH7>
2025-07-15 18:30:30 -0300

    Initialize the database, with the CodingController.cs

---
+++ CodingTracker.Niasua/Data/CodingController.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using CodingTracker.Niasua.Models;
+using CodingTracker.Niasua.Configuration;
+
+
+namespace CodingTracker.Niasua.Data;
+
+internal static class CodingController
+{
+    public static void InitializeDatabase()
+    {
+        using var connection = new SqliteConnection(AppConfig.GetConnectionString());
+
+        var tableQuery = @"CREATE TABLE IF NOT EXISTS coding_sessions (
+                            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            StartTime TEXT NOT NULL,
+                            EndTime TEXT NOT NULL,
+                            DurationHours REAL NOT NULL
+                        );";
+
+        connection.Execute(tableQuery);
+    }
+}
