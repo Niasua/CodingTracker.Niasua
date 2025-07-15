b6af26bbd63d7ea23160758774f5df57f7b1d1da
niasua <nsuar@DESKTOP-G227FH7>
2025-07-15 18:59:43 -0300

    The TableDisplay class has a ShowSessions method

---
+++ CodingTracker.Niasua/UI/TableDisplay.cs
@@ -0,0 +1,32 @@
+﻿using Spectre.Console;
+using CodingTracker.Niasua.Models;
+
+namespace CodingTracker.Niasua.UI
+{
+    internal static class TableDisplay
+    {
+        internal static void ShowSessions(List<CodingSession> sessions)
+        {
+            var table = new Table();
+            table.Border(TableBorder.Rounded);
+            table.AddColumn("[yellow]ID[/]");
+            table.AddColumn("[cyan]Start Time[/]");
+            table.AddColumn("[cyan]End Time[/]");
+            table.AddColumn("[green]Duration (hrs)[/]");
+
+            foreach (var session in sessions)
+            {
+                table.AddRow(
+                    session.Id.ToString(),
+                    session.StartTime.ToString("dd-mm-yyyy HH:mm"),
+                    session.EndTime.ToString("dd-mm-yyyy HH:mm"),
+                    session.DurationHours.ToString("0.##")
+                    );
+            }
+
+            AnsiConsole.Write(table);
+            AnsiConsole.MarkupLine("[grey]Pres any key to continue...[/]");
+            Console.ReadKey();
+        }
+    }
+}
