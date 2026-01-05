eec7dcdffb09ec076881aef23bc3681c552addc5
niasua <nsuarez2410@gmail.com>
2026-01-05 20:08:35 -0300

    unit tests completed

--- CodingTracker.Tests/ValidatorTests.cs
+++ CodingTracker.Tests/ValidatorTests.cs
@@ -9,7 +9,7 @@ public class ValidatorTests
     [InlineData("24/10/2004", false)] // invalid
     [InlineData("Just letters", false)] // invalid
     [InlineData("32-10-2004 00:00", false)] // invalid
-    public void IsValidDateTime_FormatIsCorrect_ReturnsTrue(string input, bool expected)
+    public void IsValidDateTime_ValidatesInput_Correctly(string input, bool expected)
     {
         // Arrange
         string dateTimeFormat = "dd-MM-yyyy HH:mm";
@@ -18,6 +18,54 @@ public class ValidatorTests
         var result = Validator.IsValidDateTime(input, dateTimeFormat, out DateTime date);
 
         // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("24-10-2004 10:00", "24-10-2004 12:00", true)]
+    [InlineData("24-10-2004 10:00", "24-10-2004 10:00", true)]
+    [InlineData("24-10-2004 12:00", "24-10-2004 10:00", false)]
+    public void IsValidSessionTimes_ValidatesLogic_Correctly(string startStr, string endStr, bool expected)
+    {
+        // Arrange
+        var start = DateTime.Parse(startStr);
+        var end = DateTime.Parse(endStr);
+
+        // Act
+        var result = Validator.IsValidSessionTimes(start, end);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("3", 3, true)]
+    [InlineData("0", 0, true)]
+    [InlineData("abc", 0, false)]
+    [InlineData("12.5", 0, false)]
+    public void IsValidInt_ValidatesLogic_Correctly(string input, int expectedId, bool expectedResult)
+    {
+        // Act 
+        var result = Validator.IsValidInt(input, out int id);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+
+        // int verification
+        if (expectedResult)
+        {
+            Assert.Equal(expectedId, id);
+        }
+    }
+
+    [Theory]
+    [InlineData("ASC", true)]
+    [InlineData("DESC", true)]
+    [InlineData("XYZ", false)]
+    public void IsValidOrder_ValidatesLogic_Correctly(string input, bool expected)
+    {
+        var result = Validator.IsValidOrder(input);
+
         Assert.Equal(expected, result);
     }
 }
